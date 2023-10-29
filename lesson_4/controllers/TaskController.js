08a98e5f56e86fa287fd7dc3847667e742de0f51
Maxim Kabanov <kabanov.maks.frontend@gmail.com>
2023-10-29 17:10:18 +0300

    Updated service and controller

--- lesson_4/controllers/TaskController.js
+++ lesson_4/controllers/TaskController.js
@@ -1,67 +1,31 @@
-// Базовый url
-const baseUrl = 'http://37.220.80.108/tasks'
-
 export default class TaskController {
     constructor(service) {
         this.service = service
     }
 
-    async getAllTasks() {
+    getAllTasks = async () => {
         const response = await this.service.getAllTasks()
         return response
     }
 
 
-    async getTaskById(id) {
+    getTaskById = async (id) => {
         const response = await this.service.getTaskById(id)
         return response
     }
 
-    async deleteTaskById(id) {
+    deleteTaskById = async (id) => {
         const response = await this.service.deleteTaskById(id)
         return response
     }
 
-    async addTask(task) {
+    addTask = async (task) => {
         const response = await this.service.addTask(task)
         return response
     }
 
-    async updateTaskById(id, task) {
+    updateTaskById = async (id, task) => {
         const response = await this.service.updateTaskById(id, task)
         return response
     }
 }
-// const serviceFetchTasks = new TaskController(serviceFetchTasks)
-// const serviceXMLTasks = new TaskController(serviceXMLTasks)
-
-/*
-// In src/controllers/workoutController.js
-const getAllWorkouts = (req, res) => {
-    res.send('Get all workouts');
-};
-
-const getOneWorkout = (req, res) => {
-    res.send('Get an existing workout');
-};
-
-const createNewWorkout = (req, res) => {
-    res.send('Create a new workout');
-};
-
-const updateOneWorkout = (req, res) => {
-    res.send('Update an existing workout');
-};
-
-const deleteOneWorkout = (req, res) => {
-    res.send('Delete an existing workout');
-};
-
-module.exports = {
-    getAllWorkouts,
-    getOneWorkout,
-    createNewWorkout,
-    updateOneWorkout,
-    deleteOneWorkout,
-};
-*/
--- lesson_4/services/ServiceFetchTasks.js
+++ lesson_4/services/ServiceFetchTasks.js
@@ -1,52 +1,85 @@
 export default class ServiceFetchTasks {
-    // GET - Работает
-    static async getAllTasks() {
+    constructor(_baseUrl) {
+        this._baseUrl = _baseUrl;
+    }
+
+    // GET
+    getAllTasks = async () => {
         try {
-            const response = await fetch('http://37.220.80.108/tasks')
-                .then(response => response.json())
-            return response
+            const response = await fetch(`${this._baseUrl}`).then(response => response.json());
+            return response;
         } catch (error) {
-            console.error(error)
+            console.log(`Не удалось получить все задачи - произошла ошибка: ${error}`)
+
+            return null
         }
     }
 
-    // GET/id - Работает
-    static async getTaskById(id) {
-        const response = await fetch(`http://37.220.80.108/tasks/${id}`)
-            .then(response => response.json())
+    // GET/id
+     getTaskById = async (id) => {
+        try {
+            const response = await fetch(`${this._baseUrl}/${id}`)
+                .then(response => response.json());
 
-        return response
+            return response;
+        } catch (error) {
+            console.log(`Не удалось получить задачу по id - произошла ошибка: ${error}`)
+
+            return null
+        }
+
     }
 
-    // DELETE - Работает
-    static async deleteTaskById(id) {
-        const response = await fetch(`http://37.220.80.108/tasks/${id}`, {
-            method: 'DELETE',
-        })
-        return response
+    // DELETE
+    deleteTaskById = async (id) => {
+        try {
+            const response = await fetch(`${this._baseUrl}/${id}`, {
+                method: 'DELETE',
+            });
+            return response;
+        } catch (error) {
+            console.log(`Не удалось удалить задачу по id - произошла ошибка ${error}`)
+
+            return null
+        }
+
     }
 
-    // POST - Работает
-    static async addTask(task) {
-        const response = await fetch('http://37.220.80.108/tasks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(task),
-        })
-        return response
+    // POST
+    addTask = async (task) => {
+        try {
+            const response = await fetch(`${this._baseUrl}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(task),
+            });
+            return response;
+        } catch (error) {
+            console.log(`Не удалось добавить задачу - произошла ошибка ${error}`)
+
+            return null
+        }
+
     }
 
-    // PATCH - Работает
-    static async updateTaskById(id, task) {
-        const response = await fetch(`http://37.220.80.108/tasks/${id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(task),
-        })
-        return response
+    // PATCH
+     updateTaskById = async (id, task) => {
+        try {
+            const response = await fetch(`${this._baseUrl}/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(task),
+            });
+            return response;
+        } catch (error) {
+            console.log(`Не удалось обновить задачу - произошла ошибка ${error}`)
+
+            return null
+        }
+
     }
 }
